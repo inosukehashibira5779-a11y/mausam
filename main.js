fd0b8446ca3334de9285cc0a6221452c3aee55b6
inosukehashibira5779-a11y <inosukehashibira5779@gmail.com>
2026-08-23 14:39:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function setValue(selector, value, { parent = document } = {}) {
 }
 
 function getIconUrl(iconCode) {
-    return `./icons/${ICON_MAP.get(iconCode)}.svg`
+    return `./public/icons/${ICON_MAP.get(iconCode)}.svg`
 }
 
 const currentIcon = document.querySelector("[data-current-icon]")
