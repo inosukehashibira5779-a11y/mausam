8c97c164133e17decee46aa322bc2fd979b6e0d8
inosukehashibira5779-a11y <inosukehashibira5779@gmail.com>
2026-08-23 15:30:40 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -37,7 +37,7 @@ function setValue(selector, value, { parent = document } = {}) {
 }
 
 function getIconUrl(iconCode) {
-    return `icons/${ICON_MAP.get(iconCode)}.svg`
+    return `./icons/${ICON_MAP.get(iconCode)}.svg`
 }
 
 const currentIcon = document.querySelector("[data-current-icon]")
