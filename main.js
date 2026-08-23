0a7bfcb21bfd52d8b690ae4efc74015e9e029aca
inosukehashibira5779-a11y <inosukehashibira5779@gmail.com>
2026-08-23 14:00:16 +0530

    Corrected the weather is file import in main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import "./style.css"
-import { getWeather } from "./weather"
+import { getWeather } from "./weather.js"
 import { ICON_MAP } from "./iconMap"
 
 navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
